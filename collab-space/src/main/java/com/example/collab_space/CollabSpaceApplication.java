82c1aac624855f56706b50d21d0e585c381a5be5
Prasad Shinde <prasadshinde1305@gmail.com>
2026-01-03 12:37:18 +0530

    Initial Spring Boot project setup

---
+++ collab-space/src/main/java/com/example/collab_space/CollabSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.collab_space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabSpaceApplication.class, args);
+	}
+
+}
---
+++ collab-space/src/test/java/com/example/collab_space/CollabSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collab_space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
