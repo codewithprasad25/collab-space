93d501f82477b483e924b77c801e172af15cb4e8
Prasad Shinde <prasadshinde1305@gmail.com>
2026-01-17 16:35:51 +0530

    Remove old root backend files, keep backend module only

--- backend/src/main/java/com/example/collab_space/enums/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.collab_space.enums;
-
-
-public enum Role {
-    Admin,
-    Member
-}
--- backend/src/main/java/com/example/collab_space/requestDto/OtpVerificationDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.collab_space.requestDto;
-
-import lombok.Data;
-
-@Data
-public class OtpVerificationDto {
-    String email;
-    int otp;
-}
