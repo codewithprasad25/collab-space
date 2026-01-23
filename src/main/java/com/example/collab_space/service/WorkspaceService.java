ce3843e6eb841f1268ec7b1902d95fb80a43a5ac
Prasad Shinde <prasadshinde1305@gmail.com>
2026-01-23 19:21:23 +0530

    Workspaceservice

--- src/main/java/com/example/collab_space/service/WorkspaceService.java
+++ src/main/java/com/example/collab_space/service/WorkspaceService.java
@@ -1,10 +1,21 @@
 package com.example.collab_space.service;
 
+import com.example.collab_space.model.User;
+import com.example.collab_space.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WorkspaceService {
-    public void createWorkspace(String workspaceName, String loggedInEmail) {
 
+    @Autowired
+    UserRepository userRepository;
+
+    public void createWorkspace(String workspaceName, String loggedInEmail) {
+        User user = userRepository.findByEmail(loggedInEmail);
+
+        if(user == null){
+            throw new RuntimeException("User not found");
+        }
     }
 }
